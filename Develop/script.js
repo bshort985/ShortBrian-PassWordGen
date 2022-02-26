5a429ad0d98320cbfe1f72297275262b69df95ea
bshor <bshort985@gmail.com>
2022-02-26 15:41:12 -0500

    first commit added array

---
+++ Develop/script.js
@@ -0,0 +1,48 @@
+//create array of charactors 
+
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var charactorsLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var charactorsUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "J", "K", "L", "M",
+"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+      
+
+//FUNCTION THAT PROMPTS USER FOR PASSWORD OPTIONS
+
+
+
+//conditonal statements that check if they dont include certin charators
+
+
+//cerate var that stores user options in an object
+
+
+//function that generates random number stord in the above var
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+  document.querySelector()
+
+  
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+//
+
+// Add event listener to generate button
+var generateBtn = document.querySelector("#generate")
+generate.addEventListener("click", writePassword);
+
+writePassword();
