ad3914ccf1e8b72788ad241e695a28d58105a42e
bshor <bshort985@gmail.com>
2022-02-28 14:41:33 -0500

    added user input function

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,65 @@
 //create array of charactors 
 
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+
 var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 var lettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "J", "K", "L", "M",
 "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
 
 //FUNCTION THAT PROMPTS USER FOR PASSWORD OPTIONS
+
+function getPasswordOptions(){
+var length = parseInt(
+  prompt("How long would you like yopur password to be?")
+)
+//conditonal statement to ensure proper length
+
+if (length < 8 || length > 128){
+  alert("Password must be at least eight charactors long and less than 128 charactors.")
+  return null;
+}
+
+//cerate var that stores user options in an object
+
+var speacialCharactors = confirm("Click OK to confim special charactors.")
+var numericCharactors = confirm("Click OK to confim numeric charactors.")
+var lowerCase = confirm("Click OK to confirm lower case charactors.")
+var upperCase = confirm ("Click OK to confirm upper case charactors.")
+
+//coditional statements to check if the password does not include certin charactors
+
+if ( 
+  speacialCharactors === false &&
+  numericCharactors === false &&
+  lowerCase === false &&
+  upperCase === false ){
+    alert("Must contain at least 1 special, numeric, and upper case charactor.")
+    return null;
+  }
+
+
+
+//object to store user input
+var getPasswordOptions = {
+  length: length,
+  speacialCharactors: speacialCharactors,
+  numericCharactors: numericCharactors,
+  lowerCase: lowerCase,
+  upperCase: upperCase
+}
+
+return getPasswordOptions;
+
+};
+
+
+
+
 
 function generatePassword(){
   var options = getPasswordOptions()
@@ -22,23 +73,12 @@ function generatePassword(){
   if (!options) return null
 }
 
-//conditonal statements that check if they dont include certin charators
-
-if (length < 8){
-  alert("Password must be at least eight charactors long")
-}
 
 
 
 
-//cerate var that stores user options in an object
 
-var speacialCharactors = confirm("Click OK to confim special charactors.")
-var numericCharactors = confirm("Click OK to confim numeric charactors.")
-var lowerCase = confirm("Click OK to confirm lower case charactors.")
-var upperCase = confirm ("Click OK to confirm upper case charactors.")
 
-//function that generates random number stord in the above var
 
 
 
